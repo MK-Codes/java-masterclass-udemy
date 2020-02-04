6a7f68ac21f24fbf6939be854ad7c819a1c62df2
Matthew Kaye <mk@spookie.localdomain>
2020-02-04 18:28:16 +0000

    Completed primitive vars, hello world, and ints + concat

---
+++ src/com/mkcodes/floats_chars_doubles_shorts/FloatsCharsDoublesShorts.java
@@ -0,0 +1,32 @@
+package com.mkcodes.floats_chars_doubles_shorts;
+
+public class FloatsCharsDoublesShorts {
+
+	public static void main(String[] args) {
+
+		int myInt = 12;
+		int intMax = Integer.MAX_VALUE;
+		byte myByte = 23;
+		byte byteMax = Byte.MAX_VALUE;
+		float myFloat = 23.4f;
+		float floatMax = Float.MAX_VALUE;
+		double myDouble = 122.3;
+		double doubleMax = Double.MAX_VALUE;
+		short myShort = 239;
+		short shortMax = Short.MAX_VALUE;
+		char myChar = 'M';
+
+		System.out.println("My int: " + myInt);
+		System.out.println("My byte: " + myByte);
+		System.out.println("My float: " + myFloat);
+		System.out.println("My double: " + myDouble);
+		System.out.println("My short: " + myShort);
+		System.out.println("My char: " + myChar);
+
+		System.out.println("Integers (int) goes to " + intMax + ".");
+		System.out.println("Bytes go to " + byteMax + ".");
+		System.out.println("Floats go to " + floatMax + " (standard notation).");
+		System.out.println("Doubles go to " + doubleMax + " (standard notation).");
+		System.out.println("Shorts go to " + shortMax + ".");
+	}
+}
---
+++ src/com/mkcodes/ints_and_concatenation/IntsAndConcatenation.java
@@ -0,0 +1,24 @@
+package com.mkcodes.ints_and_concatenation;
+
+public class IntsAndConcatenation {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Variables = containers
+		 * 
+		 * String = text
+		 * Integer = number (whole num)
+		 */
+
+		String myName = "MK";
+		String myCat = "Totoro";
+		String myMum = "mildly offensive";
+		
+		int myAge = 27;
+		int myCatAge = 2663;
+		
+		System.out.println(myName + " has been around for " + myAge + " years. Woosh.");
+		System.out.println(myCat + "'s age is " + myCatAge + ". She's surprisingly spritely.");
+	}
+}
