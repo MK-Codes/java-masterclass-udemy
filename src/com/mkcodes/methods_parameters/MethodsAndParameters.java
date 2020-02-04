c9fc6d50df483b35026f199d8d5e97436588e23b
Matthew Kaye <hello@mattycodes.co.uk>
2020-02-04 20:11:45 +0000

    added methods and parameters

---
+++ src/com/mkcodes/methods_parameters/MethodsAndParameters.java
@@ -0,0 +1,23 @@
+package com.mkcodes.methods_parameters;
+
+public class MethodsAndParameters {
+
+	public static void main(String[] args) {
+
+		myName("Matthew Kaye");
+		addNums(4, 3);
+		anotherMethod();
+	}
+
+	public static void myName(String myName) {
+		System.out.println(myName);
+	}
+
+	public static void addNums(int a, int b) {
+		System.out.println(a + b);
+	}
+
+	public static void anotherMethod() {
+		System.out.println("Greedy for methods");
+	}
+}
