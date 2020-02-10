d1c740317ce1126856479f953fe12702a6bc584b
Matthew <hello@mattycodes.com>
2020-02-10 09:45:56 +0000

    adding multi-exception handling (multi-catch)

--- src/com/mkcodes/error_handling/ErrorHandling.java
+++ src/com/mkcodes/error_handling/ErrorHandling.java
@@ -19,10 +19,27 @@ public class ErrorHandling {
 			} catch (NumberFormatException e) {
 				System.out.println("Exception message: " + e.getMessage());
 				System.out.println(argument + " is not a number");
+			} catch (Exception e) {
+
 			}
 		}
 
 		System.out.println("Sum is: " + sum);
+
+		int[] number = { 4, 8, 16, 32, 64, 128, 256, 512 };
+		int[] denom = { 2, 0, 4, 4, 0, 8 };
+
+		for (int i = 0; i < number.length; i++) {
+			try {
+				System.out.println(number[i] + " / " + denom[i] + " is " + number[i] / denom[i]);
+			} catch (ArithmeticException e) {
+				System.out.println("Can't divide by zero");
+			} catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("No matching element found");
+			} finally {
+				System.out.println("Running");
+			}
+		}
 	}
 
 }
