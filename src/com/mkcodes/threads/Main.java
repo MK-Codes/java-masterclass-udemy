83842292355aca95a9a5e36fe8824960ba980f24
= <=>
2020-03-02 14:36:33 +0000

    adding threads - multiple threads

--- src/com/mkcodes/threads/Main.java
+++ src/com/mkcodes/threads/Main.java
@@ -5,16 +5,32 @@ public class Main {
 	public static void main(String[] args) {
 
 		System.out.println("Main thread starting.");
-		MyThread thread = new MyThread("Thread#01");
+		MyThread thread1 = new MyThread("Thread#01");
+		MyThread thread2 = new MyThread("Thread#02");
+		MyThread thread3 = new MyThread("Thread#03");
+		MyThread thread4 = new MyThread("Thread#04");
 
-		Thread newThread = new Thread(thread);
-		newThread.start();
+		for (int i = 0; i < 50; i++) {
+			System.out.print(".");
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Main thread interrupted");
+				e.printStackTrace();
+			}
+		}
+		System.out.print("Main thread ending");
+
+//		Thread newThread = new Thread(thread);
+//		newThread.start();
 	}
 
 }
 
 class MyThread implements Runnable {
 
+	Thread thread;
 	String myThread;
 
 	@Override
@@ -36,6 +52,8 @@ class MyThread implements Runnable {
 	}
 
 	public MyThread(String myThread) {
+		thread = new Thread(this, myThread);
+		thread.start();
 		this.myThread = myThread;
 	}
 
