44843e047b58b04b135c8134402885658af513c7
= <=>
2020-03-04 08:16:20 +0000

    adding lambdas - multiple parameters

--- src/com/mkcodes/lambda/Main.java
+++ src/com/mkcodes/lambda/Main.java
@@ -4,8 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Drawable drawable = () -> System.out.println("Drawing");
-		drawable.draw();
+		Drawable drawable = (width, height) -> System.out.println("Drawing " + width + "x" + height);
+		drawable.draw(10, 100);
+
+		Addable addable = (a, b) -> (a + b);
+		System.out.println(addable.add(9, 14));
+		Addable addable1 = (int a, int b) -> (a + b);
+		System.out.println(addable1.add(10, 3));
 
 		// Old school
 		/*
@@ -18,5 +23,9 @@ public class Main {
 }
 
 interface Drawable {
-	public void draw();
+	public void draw(int width, int height);
+}
+
+interface Addable {
+	int add(int a, int b);
 }
