7f9af8082e7b2610b87c3fc3e7d368cbad88aba5
= <=>
2020-03-09 11:47:01 +0000

    adding JFX - text input

--- src/com/mkcodes/javafx/helloworld/Main.java
+++ src/com/mkcodes/javafx/helloworld/Main.java
@@ -7,12 +7,14 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	private Label myLabel;
 	private Label myLabel2;
+	private TextField nameTextField;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -22,13 +24,17 @@ public class Main extends Application {
 		button.setText("Click me");
 		myLabel = new Label();
 		myLabel2 = new Label();
+		nameTextField = new TextField();
 		myLabel.setText("I dare you. Click me!");
 		myLabel2.setText("Label 2!");
+		nameTextField.setPromptText("Enter your name");
 
 		button.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				myLabel2.setText("Hello, Cleveland!");
+				String name = nameTextField.getText().trim();
+				button.setText("Already clicked.");
+				myLabel2.setText("Hello, " + name + "!");
 			}
 		});
 
@@ -40,6 +46,7 @@ public class Main extends Application {
 		flowPaneRoot.getChildren().add(button);
 		flowPaneRoot.getChildren().add(myLabel);
 		flowPaneRoot.getChildren().add(myLabel2);
+		flowPaneRoot.getChildren().add(nameTextField);
 		Scene scene = new Scene(flowPaneRoot, 250, 200);
 		primaryStage.setScene(scene);
 		primaryStage.show();
