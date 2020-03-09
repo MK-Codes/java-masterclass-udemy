367ec9442cc29a79ab9db700fbcb910e7c24e166
= <=>
2020-03-09 11:24:03 +0000

    adding simple JFX helloWorld

--- src/com/mkcodes/javafx/helloworld/Main.java
+++ src/com/mkcodes/javafx/helloworld/Main.java
@@ -3,7 +3,9 @@ package com.mkcodes.javafx.helloworld;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -20,6 +22,12 @@ public class Main extends Application {
 				System.out.println("Hello, Cleveland!");
 			}
 		});
+
+		StackPane root = new StackPane();
+		root.getChildren().add(button);
+		Scene scene = new Scene(root, 250, 200);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
